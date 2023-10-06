c5021bf4f71b5bb7df833dc2de2f000176269de9
Somadina <Somadina@gmail.com>
2023-10-06 12:34:42 +0100

    Fix javascript

---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('.mailForm');
+const input = document.querySelector('input');
+const validate = document.querySelector('.validate');
+const card = document.querySelector('.card');
+const emailspan = document.querySelector('.emailspan');
+const complete = document.querySelector('.complete');
+const dismissBtn = document.getElementById('dismissBtn');
+
+const emailInput = document.querySelector('.emailInput');
+emailInput.addEventListener('input', () => {
+	function validateEmail(emailInput) {
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return emailRegex.test(emailInput.value);
+	}
+	if (validateEmail(emailInput) === false) {
+		emailInput.style.border = '1px solid hsl(4, 100%, 67%)';
+		validate.style.display = 'block';
+	} else {
+		emailInput.style.border = '1px solid hsl(231, 7%, 60%)';
+		validate.style.display = 'none';
+	}
+});
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	function validateEmail(emailInput) {
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return emailRegex.test(emailInput.value);
+	}
+
+	if (input.value == '') {
+		console.log('Please enter an email address');
+	} else if (validateEmail(input) === false) {
+		emailInput.style.border = '1px solid hsl(4, 100%, 67%)';
+		validate.style.display = 'block';
+	} else {
+		card.style.display = 'none';
+		emailspan.textContent = input.value;
+		complete.style.display = 'block';
+	}
+});
+
+dismissBtn.addEventListener('click', () => {
+	input.value = '';
+	complete.style.display = 'none';
+	card.style.display = 'flex';
+});
